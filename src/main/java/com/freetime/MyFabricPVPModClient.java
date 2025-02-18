1265cfaad9d45c21a44db7f93654958565a064f7
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-02-18 15:00:48 +0100

    a

---
+++ src/main/java/com/freetime/MyFabricPVPModClient.java
@@ -0,0 +1,11 @@
+package com.freetime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFabricPVPModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/freetime/item/ModItems.java
@@ -0,0 +1,27 @@
+package com.freetime.item;
+
+import com.freetime.MyFabricPVPMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item POWERFUL_STICK = registerItem("powerful_stick", new Item(new Item.Settings()));
+
+
+    public static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MyFabricPVPMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        MyFabricPVPMod.LOGGER.info("Registering Mod Items for " + MyFabricPVPMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(POWERFUL_STICK);
+        });
+    }
+}
