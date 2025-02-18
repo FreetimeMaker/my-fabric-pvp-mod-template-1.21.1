c1f61e086b89c158f5f3ae0250fafa1c26aef50e
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-02-18 14:24:29 +0100

    Initial commit

---
+++ src/main/java/com/freetime/MyFabricPVPModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.freetime;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MyFabricPVPModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
